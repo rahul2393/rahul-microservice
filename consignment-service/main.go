9c074939cb054162707753b31f7f79c585245357
Rahul Yadav <rahulyadavsep92@gmail.com>
2018-02-02 13:38:18 +0530

    adding vessel service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -1,65 +1,109 @@
+// consignment-service/main.go
 package main
 
 import (
-	"log"
 
 	// Import the generated protobuf code
+	"fmt"
+	"log"
+
 	pb "github.com/rahul-microservice/consignment-service/proto/consignment"
-	"golang.org/x/net/context"
+	vesselProto "github.com/rahul-microservice/vessel-service/proto/vessel"
 	micro "github.com/micro/go-micro"
+	"golang.org/x/net/context"
 )
 
-type IRepository interface {
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() ([]*pb.Consignment)
+	GetAll() []*pb.Consignment
 }
 
-type Repository struct {
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
-func (repo *Repository) GetAll() ([]*pb.Consignment) {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
 type service struct {
-	repo IRepository
+	repo Repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment,res *pb.Response)  error {
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	// Here we call a client instance of our vessel service with our consignment weight,
+	// and the amount of containers as the capacity value
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity: int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
+
+	// Save our consignment
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return err
 	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
 	res.Created = true
 	res.Consignment = consignment
 	return nil
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest,res *pb.Response) error {
-	res.Consignments = s.repo.GetAll()
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
 	return nil
 }
 
 func main() {
 
-	repo := &Repository{}
+	repo := &ConsignmentRepository{}
 
+	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
+
+		// This name must match the package name given in your protobuf definition
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
 	)
+
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
+	// Init will parse the command line flags.
 	srv.Init()
 
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	// Register handler
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
+	// Run the server
 	if err := srv.Run(); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		fmt.Println(err)
 	}
 }
